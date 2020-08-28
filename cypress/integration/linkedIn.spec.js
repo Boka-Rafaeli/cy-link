7e0790fa4386f53cd6db627984b083dd4ff5d27c
benyamin <benyamin.rafaeli@gmail.com>
2020-08-28 18:03:40 +0300

    wip

--- cypress/integration/linkedIn.spec.js
+++ cypress/integration/linkedIn.spec.js
@@ -2,26 +2,46 @@
 
 context('LinkedIn', () => {
 
-  xit('HR discovering', () => {
-    cy.fixture('credentials.json').as('user')
-    cy.get('@user').then(user => cy.performLogin(user))
-    cy.navigateToNetwork()
-    cy.scrollDownTimes(10)
-    cy.collectHR()
-  })
+    it('HR discovering', () => {
+        cy.fixture('credentials.json').as('user')
+        cy.get('@user').then(user => cy.performLogin(user))
+        cy.navigateToNetwork()
 
-  it.only('Sending request', () => {
-    cy.fixture('credentials.json').as('user')
-    cy.get('@user').then(user => cy.performLogin(user))
-    cy.sendRequest()
-  })
-
-  it('fake login', () => {
-    Cypress.config('baseUrl', '')
-    cy.visit('cypress/fake/test.html') // {failOnStatusCode: false}
-    cy.get('.ph5.pb5').then(el => {
-      const myElement = el.find('.artdeco-button__text').first().text()
-      cy.log(myElement)
+        Cypress._.times(5, (i) => {
+            cy.scrollDownTimes(2)
+            cy.collectHR(10)
+            cy.log(`🐍🐍🐍🐍🐍 cycle => ${i}`)
+        })
+        // cy.scrollDownTimes(5)
+        // cy.collectHR(20)
     })
-  })
+
+    it.only('Sending request', () => {
+        cy.fixture('credentials.json').as('user')
+        cy.get('@user').then(user => cy.performLogin(user))
+
+        // cy.fixture('hr_0.json').as('target')
+        // cy.get('@target').then(users => cy.log(`There are ${users.length} hrs.`))
+        // cy.get('@f1').then(f => cy.sendRequest(f))
+
+        cy.sendRequest('hr_0.json')
+
+        // cy.fixture('hr_0.json').as('f1')
+        // cy.get('@f1').then(f => cy.sendRequest(f))
+
+        // Cypress.Promise
+        //     .all([cy.fixture('hr_0'), cy.fixture('hr_1')])
+        //     .spread((city, country) => {
+        //     })
+
+    })
+
+    it('fake login', () => {
+        Cypress.config('baseUrl', '')
+        cy.visit('cypress/fake/test.html') // {failOnStatusCode: false}
+        cy.get('.ph5.pb5').then(el => {
+            const myElement = el.find('.artdeco-button__text').first().text()
+            cy.log(myElement)
+        })
+    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('performLogin', (user) => {
     cy.visit('login', {})
-    cy.get('#username').focus().type(user.email, {delay: 10})
+    cy.get('#username').focus().type(user.email, {delay: 10, log: false})
     cy.get('#password').focus().type(user.password, {delay: 10, log: false})
     cy.get('.btn__primary--large').click()
     cy.url().should('include', 'feed')
@@ -16,44 +16,49 @@ Cypress.Commands.add('scrollDownTimes', (scrollDownTimes) => {
     let timer
     Cypress._.times(scrollDownTimes, (i) => {
         timer = Math.floor(Math.random() * 999 + 2000)
-        cy.log(`💥💥💥💥 scrolling times => ${i + 1} from => ${scrollDownTimes} timer => ${timer}`)
+        cy.log(`💥💥💥💥💥 scrolling times => ${i + 1} from => ${scrollDownTimes}`)
         cy.get('#type-ahead-wormhole').scrollIntoView({duration: timer})
         cy.log(`wait for ${timer}`)
         cy.wait(timer)
     })
 })
 
-Cypress.Commands.add('collectHR', () => {
-    let personName, personPosition, personHref, count = 0
-    const results = []
+Cypress.Commands.add('collectHR', (contactsNumber) => {
+    let personName, personPosition, personHref, count = 0, name = 0, results = []
+    cy
+        .get('[data-control-name="pymk_profile"]')
+        .each(element => {
+            personHref = element.attr('href')
+            personName = element.find('.discover-person-card__name').text().trim()
+            personPosition = element.find('.discover-person-card__occupation').text().trim()
 
-    try {
-        cy
-            .get('[data-control-name="pymk_profile"]')
-            .each(element => {
-                personHref = element.attr('href')
-                personName = element.find('.discover-person-card__name').text().trim()
-                personPosition = element.find('.discover-person-card__occupation').text().trim()
+            if (personPosition.includes('HR') || personPosition.includes('Acquisition') || personPosition.includes('Human Resources')) {
+                count++
+                cy.log(`href => ${personHref}`)
+                cy.log(`name => ${personName}`)
+                cy.log(`position => ${personPosition}`)
+                results.push({name: personName, position: personPosition, href: personHref})
+                cy.log(`👆👆👆👆👆 ${count}`)
 
-                if (personPosition.includes('HR') || personPosition.includes('Acquisition') || personPosition.includes('Human Resources')) {
-                    count++
-                    cy.log(`👇👇👇👇👇👇`)
-                    cy.log(`href => ${personHref}`)
-                    cy.log(`name => ${personName}`)
-                    cy.log(`position => ${personPosition}`)
-                    results.push({name: personName, position: personPosition, href: personHref})
-                    cy.log(`👆👆👆👆👆👆 ${count}`)
+                if (count > contactsNumber) {
+                    cy.createFile(name, results)
+                    count = 0
+                    results = []
+                    name = name + 1
                 }
-            }).then(() => cy.writeFile('cypress/fixtures/hr.json', results))
-    } finally {
-        cy.writeFile('cypress/fixtures/hr.json', results)
-    }
+            }
+        })
 })
 
-Cypress.Commands.add('sendRequest', () => {
-    cy.fixture('hr.json').as('target')
+Cypress.Commands.add('createFile', (name, results) => {
+    cy.log(`🐲🐲🐲🐲🐲 => createFile hr_${name}.json`)
+    cy.writeFile(`cypress/fixtures/hr_${name}.json`, results)
+})
 
-    cy.get('@target').then(users => cy.log(`There are ${users.length} hrs.`))
+
+Cypress.Commands.add('sendRequest', (f) => {
+    cy.fixture(`${f}`).as('target')
+    cy.get('@target').then(users => cy.log(`🐶🐶🐶🐶🐶 found ${users.length} hrs.`))
 
     cy.get('@target').each(target => {
         cy.log(`Navigate to => : ${Cypress.config().baseUrl}${target.href}`)
@@ -62,13 +67,10 @@ Cypress.Commands.add('sendRequest', () => {
         cy.get('.ph5.pb5').then(el => {
             const myElement = el.find('.artdeco-button__text').first()
             if (myElement.text().includes('Connect')) {
-                cy.log(`Found element: ${myElement}`)
                 myElement.trigger('click')
-                cy.log(`Try Connect => 🥂🥂🥂🥂🥂`)
                 cy.contains('Done').click()
-                cy.log(`Done Connect => 🥂🥂🥂🥂🥂`)
+                cy.log(`🥂🥂🥂🥂🥂 => Connect`)
             }
-
         })
     })
 })
